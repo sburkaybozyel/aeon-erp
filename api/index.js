08a621dacb8b6c1b37164d323405d66714b960e9
sburkaybozyel <burkaybozyel@gmail.com>
2026-08-12 01:35:04 +0300

    debug: catch serverless handler errors

--- api/index.js
+++ api/index.js
@@ -1,2 +1,14 @@
 import app from '../server.js';
-export default app;
+
+export default async function handler(req, res) {
+  try {
+    return app(req, res);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+      stack: err.stack,
+      url: req.url,
+      method: req.method
+    });
+  }
+}
