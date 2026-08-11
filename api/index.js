b41df1e9f7c807a65ad084e866f943249c372903
sburkaybozyel <burkaybozyel@gmail.com>
2026-08-12 01:35:27 +0300

    fix: Vercel serverless function build resolution

--- api/index.js
+++ api/index.js
@@ -1,14 +1,3 @@
 import app from '../server.js';
 
-export default async function handler(req, res) {
-  try {
-    return app(req, res);
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-      stack: err.stack,
-      url: req.url,
-      method: req.method
-    });
-  }
-}
+export default app;
